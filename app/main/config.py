95728ad0e75ad624662b355f61407581fa1a2766
connor <connormullett@gmail.com>
2019-06-19 14:58:40 -0400

    models migrated

---
+++ app/main/config.py
@@ -0,0 +1,38 @@
+
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY', 'secretkeyv1')
+    DEBUG = False
+
+
+class Development(Config):
+    # uncomment the line below to use postgres
+    # SQLALCHEMY_DATABASE_URI = postgres_local_base
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class Testing(Config):
+    DEBUG = True
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    PRESERVE_CONTEXT_ON_EXCEPTION = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class Production(Config):
+    DEBUG = False
+
+
+app_config = dict(
+    develop=Development,
+    testing=Testing,
+    production=Production 
+)
+
+key = Config.SECRET_KEY
